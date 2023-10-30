ee472a342459ad24da4ae0fae32625eb5b4adbc2
devowais@outlook.com <devowais@outlook.com>
2023-10-30 19:19:00 +0500

    Added the feature to display country's information.

--- script.js
+++ script.js
@@ -3,13 +3,14 @@ const find = document.getElementById("find"); //Reference to the button
 const container = document.getElementById("container"); //Reference to main container
 const loader = document.getElementById("loader"); // Reference to the loader element
 
-find.addEventListener("click", function () { //button eventlistener to get the data
+find.addEventListener("click", function () {
+  //button eventlistener to get the data
   loader.style.display = "block"; // Display the loader before making the geolocation request
-  Promise.race([racePromise(), findMe()]).then(res => console.log(res));
-  
+  Promise.race([racePromise(), findMe()]).then((res) => renderCountry(res));
 });
 
-const getLoc = function () { //function to get the user location coordinates
+const getLoc = function () {
+  //function to get the user location coordinates
   if (navigator.geolocation) {
     return new Promise((resolve, reject) => {
       navigator.geolocation.getCurrentPosition(resolve, reject);
@@ -20,7 +21,8 @@ const getLoc = function () { //function to get the user location coordinates
     );
   }
 };
-const getCountry = async function () { //function to get the country thr user resides currently
+const getCountry = async function () {
+  //function to get the country thr user resides currently
   const pos = await getLoc();
   const { latitude: lat, longitude: lng } = pos.coords;
   const response = await fetch(
@@ -30,27 +32,53 @@ const getCountry = async function () { //function to get the country thr user re
   loader.style.display = "none"; // Hide the loader when the request is successful
   return data.address.country;
 };
-const findMe = async function () { //function to get the data of the current country
-  return new Promise(async(resolve, reject) => {
-    try{
+const findMe = async function () {
+  //function to get the data of the current country
+  return new Promise(async (resolve, reject) => {
+    try {
       const country = await getCountry(); //calling the getCountry function to get the country to pass to API for details
-      const data = await fetch(`https://restcountries.com/v3.1/name/${country}`); //API call for getting the country's data
+      const data = await fetch(
+        `https://restcountries.com/v3.1/name/${country}`
+      ); //API call for getting the country's data
       const dataJSON = await data.json();
       resolve(dataJSON[0]);
-    }
-    catch(err){
+    } catch (err) {
       reject(err);
     }
   });
 };
-const racePromise = function(){
+const racePromise = function () {
   return new Promise((_, reject) => {
-    setTimeout(
-      () => {
-        reject(new Error("Timeout"));
-      },
-      5000
-    )
+    setTimeout(() => {
+      reject(new Error("Timeout"));
+    }, 5000);
   });
-}
+};
 
+const renderCountry = function (data) {
+  const html = `
+
+    <div class="flag_container">
+      <div class="name">
+        <h3>${data.name.common}</h3>
+      </div>
+      <div class="flag">
+        <img src="${data.flags.png}" alt="Flag" />
+      </div>
+    </div>
+    <div class="info_container">
+      <p><span class="sub">Currency: </span>${Object.keys(data.currencies).join(
+        ", "
+      )}</p>
+      <p><span class="sub">Population: </span>${data.population}</p>
+      <p><span class="sub">Area (km²): </span>${data.area}</p>
+      <p><span class="sub">Languages: </span>${Object.values(
+        data.languages
+      ).join(", ")}</p>
+    </div>
+ `;
+
+  // container.innerHTML = html;
+  container.insertAdjacentHTML("afterbegin", html);
+  find.style.display = "none";
+};
