a87a84f3b2cc81254bc06926217ff91736792687
devowais@outlook.com <devowais@outlook.com>
2023-10-30 18:32:10 +0500

    Promisified the findMe method and made it race with timeout.

--- script.js
+++ script.js
@@ -1,46 +1,56 @@
-'use strict'
-const find = document.getElementById('find');
-const container = document.getElementById('container');
-const loader = document.getElementById('loader'); // Reference to the loader element
+"use strict";
+const find = document.getElementById("find"); //Reference to the button
+const container = document.getElementById("container"); //Reference to main container
+const loader = document.getElementById("loader"); // Reference to the loader element
 
-find.addEventListener('click', function () {
- loader.style.display = 'block'; // Display the loader before making the geolocation request
-
-  // getLoc()
-  //   .then((res) => {
-  //     console.log(res);
-  //     loader.style.display = 'none'; // Hide the loader when the request is successful
-  //   })
-  //   .catch((err) => {
-  //     console.log(err.message);
-  //     loader.style.display = 'none'; // Hide the loader when there's an error
-  //   });
- findMe();
+find.addEventListener("click", function () { //button eventlistener to get the data
+  loader.style.display = "block"; // Display the loader before making the geolocation request
+  Promise.race([racePromise(), findMe()]).then(res => console.log(res));
+  
 });
 
-const getLoc = function () {
+const getLoc = function () { //function to get the user location coordinates
   if (navigator.geolocation) {
     return new Promise((resolve, reject) => {
       navigator.geolocation.getCurrentPosition(resolve, reject);
     });
   } else {
     return new Promise((_, reject) =>
-      reject('Geolocation is not supported by this browser')
+      reject("Geolocation is not supported by this browser")
     );
   }
 };
-const getCountry = async function(){
-  const pos = await getLoc().then(coords => coords);
+const getCountry = async function () { //function to get the country thr user resides currently
+  const pos = await getLoc();
   const { latitude: lat, longitude: lng } = pos.coords;
-     const response = await fetch (`https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${lng}&format=json`)
-     const data = await response.json();
-     loader.style.display = 'none'; // Hide the loader when the request is successful
-     return(data.address.country)
-     
+  const response = await fetch(
+    `https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${lng}&format=json`
+  ); //API call for getting the country
+  const data = await response.json();
+  loader.style.display = "none"; // Hide the loader when the request is successful
+  return data.address.country;
+};
+const findMe = async function () { //function to get the data of the current country
+  return new Promise(async(resolve, reject) => {
+    try{
+      const country = await getCountry(); //calling the getCountry function to get the country to pass to API for details
+      const data = await fetch(`https://restcountries.com/v3.1/name/${country}`); //API call for getting the country's data
+      const dataJSON = await data.json();
+      resolve(dataJSON[0]);
+    }
+    catch(err){
+      reject(err);
+    }
+  });
+};
+const racePromise = function(){
+  return new Promise((_, reject) => {
+    setTimeout(
+      () => {
+        reject(new Error("Timeout"));
+      },
+      5000
+    )
+  });
 }
-const findMe = async function(){
-const country = await getCountry();
-const data = await fetch(`https://restcountries.com/v3.1/name/${country}`);
-const dataJSON = await data.json();
-console.log(dataJSON[0])
-}
+
